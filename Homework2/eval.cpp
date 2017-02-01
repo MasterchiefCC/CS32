0602797d8f781bb24ac1328e8bfb397508edbfc0
MasterchiefCC <chenchenstudent@gmail.com>
2017-02-01 01:15:04 -0800

    Add files via upload

---
+++ Homework2/eval.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <stack>
+#include <cassert>
+using namespace std;
+int precedence(char ch){
+	switch (ch){
+	case '!':       
+		return 2;
+	case '&':      
+		return 1;
+	case '|':
+		return 1; 
+	default:
+		return -1;
+	}
+}
+
+
+int evaluate(string infix, const bool values[], string& postfix, bool& result) {
+	postfix = "";
+	stack<char> operatorS;
+	for (unsigned int a = 0; a < infix.size(); a++) {
+		char temp = infix[a];
+		switch (temp) {
+		case '0': case '1':case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+			postfix += temp;
+			break; case '(':
+				if (a + 1 >= infix.size() || infix[a + 1] == '|' || infix[a + 1] == '&')return 1;
+				operatorS.push(temp);
+			break;case ')':
+				while (operatorS.top() != '(') {
+					postfix += operatorS.top();
+					operatorS.pop();
+				}
+			operatorS.pop();
+			break; case '|': case '&' : case '!':
+				while (!operatorS.empty() && operatorS.top() != '('&&precedence(temp) <= precedence(operatorS.top())) {
+					postfix += operatorS.top();
+					operatorS.pop();
+				}
+				operatorS.push(temp);
+				break;
+		}
+	}
+	while (!operatorS.empty()) {
+		if (operatorS.top() == '(')return 1;
+		postfix += operatorS.top();
+		operatorS.pop();
+	}
+	if (postfix.empty())return 1;
+	stack<bool>resultS;
+	for (unsigned int a = 0; a < postfix.size(); a++) {
+		char temp = postfix[a];
+		switch (temp) {
+		case '0': case '1':case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+			resultS.push(values[temp - '0']);
+			break;
+		case '!': {
+			if (resultS.empty())return 1;
+			bool ch = resultS.top();
+			resultS.pop();
+			resultS.push(!ch);
+			break;
+		}case '|': {
+			if (resultS.empty())return 1;
+			bool val1 = resultS.top();
+			resultS.pop();
+			if (resultS.empty())return 1;
+			bool val2 = resultS.top();
+			resultS.pop();
+			resultS.push(val1 || val2);
+			break;}
+		case '&': {
+			if (resultS.empty())return 1;
+			bool val1 = resultS.top();
+			resultS.pop();
+			if (resultS.empty())return 1;
+			bool val2 = resultS.top();
+			resultS.pop();
+			resultS.push(val1&&val2);
+			break;
+			}
+		}
+	}
+	if (resultS.size() != 1)return 1;
+	result = resultS.top();
+	return 0;
+}
+
+void test1() {
+	bool ba[10] = {
+		//  0      1      2      3      4      5      6      7      8      9
+		true,  true,  true,  false, false, false, true,  false, true,  false
+	};
+	string pf;
+	bool answer;
+	//evaluate("2| 3", ba, pf, answer);
+	//assert(evaluate("2| 3", ba, pf, answer) == 0  &&  answer);
+	assert(evaluate("8|", ba, pf, answer) == 1);
+	assert(evaluate("4 5", ba, pf, answer) == 1);
+	assert(evaluate("01", ba, pf, answer) == 1);
+	assert(evaluate("()", ba, pf, answer) == 1);
+	assert(evaluate("2(9|8)", ba, pf, answer) == 1);
+	assert(evaluate("2(&8)", ba, pf, answer) == 1);
+	assert(evaluate("(6&(7|7)", ba, pf, answer) == 1);
+	assert(evaluate("", ba, pf, answer) == 1);
+	assert(evaluate("4  |  !3 & (0&3) ", ba, pf, answer) == 0
+		&& !answer);
+	assert(evaluate(" 9  ", ba, pf, answer) == 0 && !answer);
+	ba[2] = false;
+	ba[9] = true;
+	assert(evaluate("((9))", ba, pf, answer) == 0 && answer);
+	assert(evaluate("2| 3", ba, pf, answer) == 0 && !answer);
+	cout << "Passed all tests" << endl;
+}
+int main(){
+	test1();
+	bool ba[10] = {
+		//  0      1      2      3      4      5      6      7      8      9
+		true,  true,  true,  false, false, false, true,  false,true,  false
+	};
+	string pf;
+	bool result;
+	assert(evaluate("1&!(9|1&1|9) | !!!(9&1&9)", ba, pf, result) == 0 && result);
+}
---
+++ Homework2/mazestack.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stack>
+#include <string.h>
+using namespace std;
+class Coord{
+public:
+	Coord(int rr, int cc) : m_r(rr), m_c(cc) {}
+	int r() const { return m_r; }
+	int c() const { return m_c; }
+private:
+	int m_r;
+	int m_c;
+};
+
+bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec) {
+	stack<Coord> coordStack;
+	Coord init(sr, sc);
+	coordStack.push(init);
+	while (!coordStack.empty()) {
+		Coord temp = coordStack.top();
+		coordStack.pop();
+		int tr = temp.r();
+		int tc = temp.c();
+		if (tr == er&&tc == ec) return true;
+		maze[tr][tc] = 'X';
+		for(int a=-1; a<=1; a++)
+			for (int b = -1; b <= 1; b++) {
+				if (a == b)continue;
+				if (tr+a>=0 && tc+b>=0 && tr+a<nRows && tc+b<nCols&&maze[tr + a][tc + b] == '.') {
+					Coord pushin(tr + a, tc + b);
+					coordStack.push(pushin);
+				}
+			}
+	}
+	return false;
+}
+
+
+int main()
+{
+	string maze[10] = {
+		"XXXXXXXXXX",
+		"X........X",
+		"XX.X.XXXXX",
+		"X..X.X...X",
+		"X..X...X.X",
+		"XXXX.XXX.X",
+		"X.X....XXX",
+		"X..XX.XX.X",
+		"X...X....X",
+		"XXXXXXXXXX"
+	};
+
+	if (pathExists(maze, 10, 10, 6, 4, 1, 1))
+		cout << "Solvable!" << endl;
+	else
+		cout << "Out of luck!" << endl;
+}
